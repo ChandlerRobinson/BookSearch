f070ca8af0c7f1261596fe8445e4074f78fbdfdc
ChandlerRobinson <chandlerbronsonrobinson@gmail.com>
2024-12-06 00:21:56 -0500

    Added ApolloProvider to App.tsx for GraphQL integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Apollo Client setup
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // GraphQL server URL
+  cache: new InMemoryCache(), // Cache to store fetched data
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
